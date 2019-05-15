80da9971c8dc23784acc25fa6c4da92467c416dd
mazdak <nazemi.works@gmail.com>
2019-05-15 07:48:09 -0700

    query youtube api connected components

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import youtube from '../apis/youtube';
+
 import SearchBar from './SearchBar';
 
 class App extends Component {
+
+  onTermSubmit = term => {
+    youtube.get('/search', {
+      params : {
+        q : term
+      }
+    });
+  };
+
   render() {
     return (
-      <div>
-        <SearchBar/>
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit}/>
       </div>
     );
-  }
+  };
 }
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+
+  state = { term : ''};
+
+  onInputChange = e => {
+    this.setState({term : e.target.value});
+  }
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onFormSubmit(this.state.term);
+  }
+
   render() {
     return (
-      <div>
-        Seaechbar
-      </div>
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Search </label>
+            <input value={this.state.term} onChange={this.onInputChange} type="text"/>
+          </div>
+        </form>
+      </div>        
     );
   }
 }
