75d191f919833f15739cd41d93a5dba4e968702f
mazdak <nazemi.works@gmail.com>
2019-05-15 09:23:33 -0700

    video app done some tricks

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,9 +1,25 @@
 import React from 'react'
 
-const VideoDetail = () => {
+const VideoDetail = ({video}) => {
+  if(!video){
+    return <div>LOADING</div>
+  }
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
   return (
     <div>
-      sss
+      <div className="ui embed">
+        <iframe src={videoSrc} title="video Player"/>
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">
+          {video.snippet.title}
+        </h4>
+        <p>
+          {video.snippet.description}
+        </p>
+      </div>
     </div>
   )
 }
