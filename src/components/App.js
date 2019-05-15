b42f920f835a12e9b3cf1fd3f3b9c997a9a30117
mazdak <nazemi.works@gmail.com>
2019-05-15 08:38:56 -0700

    make nesting cb child to parent

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,11 @@ import youtube from '../apis/youtube';
 
 import SearchBar from './SearchBar';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends Component {
 
-  state = { videos : []};
+  state = { videos : [] , selectedVideo : null};
 
   onTermSubmit = async term => {
     const response = await youtube.get('/search', {
@@ -17,11 +18,18 @@ class App extends Component {
     this.setState({videos : response.data.items});
   };
 
+  onVideoSelect = video => {
+    console.log(video);
+  }
+
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit}/>
-        <VideoList videos={this.state.videos}/>
+        <VideoList onVideoSelect={this.onVideoSelect} 
+          videos={this.state.videos}
+        />
+        <VideoDetail/>
       </div>
     );
   };
---
+++ src/components/VideoDetail.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoDetail = () => {
+  return (
+    <div>
+      sss
+    </div>
+  )
+}
+
+export default VideoDetail;
